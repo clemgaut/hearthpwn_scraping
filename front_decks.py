04c4f0775540097fbc4edf1137eb2daba85e613f
Clément Gautrais <clemgaut@gmail.com>
2016-11-08 15:42:02 +0100

    Adds command line and restructure sql

--- front_decks.py
+++ front_decks.py
@@ -5,12 +5,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import re
-import json
 import sys, os
+import argparse
 import jsonpickle
 import datetime
 import sqlite3
 from objects import FrontDeckInfo
+
+FRONT_DECKS_TABLE_NAME = "front_decks"
+DECKS_TABLE_NAME = "decks"
 
 
 def extract_front_decks(phantom_js_path=r"./phantomjs-2.1.1-windows/bin/phantomjs.exe"):
@@ -51,20 +54,77 @@ def extract_front_decks(phantom_js_path=r"./phantomjs-2.1.1-windows/bin/phantomj
 def save_to_json(top_decks, dir = "", file_prefix="hearthpwn_top_decks_"):
     """
     Save the current decks into a json file named prefix+timestamp.json
-    :param top_decks: The decks to save. A list of dict
+    :param top_decks: The decks to save. A list of FrontDeckInfo
     :param file_prefix: The json file prefix
     :return:
     """
     with open(os.path.join(dir, file_prefix) + str(datetime.datetime.now().timestamp()) + ".json", "w") as f:
         f.write(jsonpickle.encode(top_decks))
 
+
+def save_to_sqlite(top_decks, db_file="hearth_pwn_scrap.db"):
+    """
+    Saves the front decks to an sqlite table. The table is created if it does not exist.
+    :param top_decks: The decks to save. A list of FrontDeckInfo
+    :param db_file: The database name
+    :return:
+    """
+    create_sqlite_table()
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+    timestamp = datetime.datetime.now().timestamp()
+
+    for deck in top_decks:
+        id_deck = c.execute("SELECT id FROM " + DECKS_TABLE_NAME + " WHERE link='" + deck.link + "'")
+        if not id_deck:
+            c.execute("INSERT INTO ? (link) VALUES (?)", (DECKS_TABLE_NAME, deck.link))
+        c.execute("INSERT INTO " + FRONT_DECKS_TABLE_NAME +
+                " (deck_id, hs_class, rating, last_edit, views, comments, timestamp) "
+                "values ( (SELECT id FROM " + DECKS_TABLE_NAME + " WHERE link='" + deck.link + "'), ?, ?, ? ,?, ?, ?)",
+                (deck.hs_class, deck.rating, deck.last_edit, deck.views, deck.comments, timestamp))
+    c.close()
+    conn.commit()
+    conn.close()
+
+
+def create_sqlite_table():
+    """
+    Create the sqlite tables for decks and front decks description if they do not exist
+    :return:
+    """
+    conn = sqlite3.connect("hearth_pwn_scrap.db")
+    c = conn.cursor()
+    # We create the table of hearthpwn decks if it does not exists
+    c.execute("CREATE TABLE IF NOT EXISTS " + DECKS_TABLE_NAME +
+              " (id INTEGER PRIMARY KEY,link TEXT)")
+
+    c.execute("CREATE TABLE IF NOT EXISTS " + FRONT_DECKS_TABLE_NAME +
+              " (id INTEGER PRIMARY KEY,deck_id INTEGER, hs_class TEXT, rating INTEGER,last_edit TEXT, views TEXT, comments INTEGER, timestamp INTEGER,"
+              "FOREIGN KEY(deck_id) REFERENCES " + DECKS_TABLE_NAME + "(id))")
+    c.close()
+    conn.commit()
+    conn.close()
+
+
 if __name__ == "__main__":
     top_decks = []
-    if len(sys.argv) > 1:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--phantomjsPath", help="Path to the phantomJS exe")
+    parser.add_argument("--storagePath", help="Path to the storage file (db file if sqlite and directory if json)")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--sqlite", help="Activate storage in sqlite database", action="store_true")
+    group.add_argument("--json", help="Activate storage in json format (default option)", action="store_true")
+    args = parser.parse_args()
+
+    storage_func = save_to_json
+    if args.sqlite:
+        storage_func = save_to_sqlite
+
+    if args.phantomjsPath:
         top_decks = extract_front_decks(phantom_js_path=sys.argv[1])
     else:
         top_decks = extract_front_decks()
-    if len(sys.argv) > 2:
-        save_to_json(top_decks, sys.argv[2])
+    if args.storagePath:
+        storage_func(top_decks, args.storagePath)
     else:
-        save_to_json(top_decks)
+        storage_func(top_decks)
